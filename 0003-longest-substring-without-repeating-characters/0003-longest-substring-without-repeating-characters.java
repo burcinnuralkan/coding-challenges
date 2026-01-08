b9748cd45bd91c23d87e3496037a081b7d83c349
Burçinnur Alkan <burcinnuralkan@gmail.com>
2026-01-08 15:16:07 +0300

    Time: 5 ms (85.26%), Space: 45.4 MB (75.95%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -6,9 +6,9 @@ class Solution {
         for(int right = 0, left = 0; right < n; right++){
             char curr = s.charAt(right);
             if(map.containsKey(curr)){
-                left = Math.max(map.get(curr),left);
+                left = Math.max(map.get(curr), left);
             }
-            map.put(curr,right+1);
+            map.put(curr, right + 1);
             maxLen = Math.max(maxLen, right - left + 1);
         }
         return maxLen;
